5028acd5e48f48cfbad0a598d5e054f4f9392a47
Trey Turner <treyturner@Williams-MacBook-Air.local>
2022-06-10 13:21:14 -0500

    more changes'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,3 +4,10 @@ function onLoad() {
     let loader = document.getElementById("loader");
     loader.style.display = "none";
 }
+function skillsShowContent(id) {
+    document.getElementById(id).classList.remove("skills-hide");
+}
+
+function skillsHideContent(id) {
+    document.getElementById(id).classList.add("skills-hide");
+}
